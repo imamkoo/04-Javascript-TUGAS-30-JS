6d2964e6378cb2edf0ca775701f1ab3b6f79cd6a
Imamkoo <imammm.kooo@gmail.com>
2021-12-23 12:23:10 +0700

    Tugas 30 Javascript

---
+++ tugas30.js
@@ -0,0 +1,9 @@
+function regex(value) {
+    let data = value.match(/e|s/g);
+
+    console.log(data);
+}
+
+regex(
+    "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
